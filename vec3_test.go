b801640e5b9dbed9766b773b2c4fb4b325fbae04
mrkuc <2574472+mrkuc@users.noreply.github.com>
2020-01-26 23:59:06 +0900

    Added Vec3 test

--- vec3_test.go
+++ vec3_test.go
@@ -12,3 +12,30 @@ func TestAdd(t *testing.T) {
 		t.Fatal("failed Add")
 	}
 }
+
+func TestSub(t *testing.T) {
+	v1 := &Vec3{1, 2, 3}
+	v2 := &Vec3{4, 5, 6}
+	v1.Sub(v2)
+	if !(*v1 == Vec3{-3, -3, -3}) {
+		t.Fatal("failed Sub")
+	}
+}
+
+func TestMul(t *testing.T) {
+	v1 := &Vec3{1, 2, 3}
+	v2 := &Vec3{4, 5, 6}
+	v1.Mul(v2)
+	if !(*v1 == Vec3{4, 10, 18}) {
+		t.Fatal("failed Mul")
+	}
+}
+
+func TestDiv(t *testing.T) {
+	v1 := &Vec3{1, 2, 3}
+	v2 := &Vec3{4, 5, 6}
+	v1.Div(v2)
+	if !(*v1 == Vec3{0.25, 0.4, 0.5}) {
+		t.Fatal("failed Div")
+	}
+}
